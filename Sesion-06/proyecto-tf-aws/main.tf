49f462b982c8524517fd964b8c959ea941609059
Jorge Medina <jorge.medina@kronops.com.mx>
2022-11-17 19:29:18 -0600

    Se define bloque terraform para proveedor aws.

--- Sesion-06/proyecto-tf-aws/main.tf
+++ Sesion-06/proyecto-tf-aws/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.40.0"
+    }
+  }
+}
+
 provider "aws" {
   region     = var.region
   access_key = var.access_key
