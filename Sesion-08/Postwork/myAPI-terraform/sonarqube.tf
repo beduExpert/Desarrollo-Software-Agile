78f12a7736d6224fad54c3c5c2c6f4cdc8d48212
Jorge Armando Medina <jorgearma1982@gmail.com>
2022-11-25 21:23:23 -0600

    Update sonarqube.tf

--- Sesion-08/Postwork/myAPI-terraform/sonarqube.tf
+++ Sesion-08/Postwork/myAPI-terraform/sonarqube.tf
@@ -6,6 +6,10 @@ resource "docker_container" "sonarqube" {
   name = "sonarqube"
   image = "${docker_image.sonarqube.latest}"
   ports {
+    internal = 9000
+    external = 9000
+  }
+  ports {
     internal = 9092
     external = 9092
   }
